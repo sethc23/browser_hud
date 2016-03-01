61eeecbd31858a90f3605a9899cddf3059c21d2c
sethc23 <sethc23@users.noreply.github.com>
2016-03-01 14:36:48 -0500

    added functionality for effectively minimizing hud to persistent icon; added functionality for switching between pager and floating display

--- browser_hud.py
+++ browser_hud.py
@@ -2,13 +2,26 @@
 import ipywidgets as widgets
 from IPython.display import display,HTML,clear_output
 import json
+from time import sleep
+from IPython import get_ipython
 
 class browser_hud:
     
-    def __init__(self):
+    def __init__(self,display_type=''):
         self._widget = widgets.Box(_dom_classes=['inspector'])
         self._widget.overflow_x = 'scroll'
         self._widget.overflow_y = 'scroll'
+        self._widget.background_color='#444648'
+        if display_type:
+            self.display_type = display_type
+        else:
+            self.display_type = 'float'
+
+    def set_color_scheme(self,_scheme,_objects=[]):
+        for it in _objects:
+            for k,v in _scheme.iteritems():
+                setattr(it,k,v)
+        return _objects
 
     def set_css(self):
         """
@@ -44,8 +57,24 @@ class browser_hud:
                                                           ])
                                                 ])
                                      )
-        
-    
+        get_ipython().run_cell_magic(u'javascript', u'',
+                                     u"""
+                                     $('button.update_btn_1').css('visibility', 'hidden');
+                                     $('.widget-label').css('color', '#53D2FF');
+                                     """)
+        # open external -- #pager-button-area > a:nth-child(1)
+        # close pager -- pager-button-area > a:nth-child(2)
+        get_ipython().run_cell_magic(u'javascript', u'',
+                                     u"""
+                                     var external_button = $('#pager-button-area > a:nth-child(1)');
+                                     external_button.onclick = external_button_click;
+                                    function external_button_click() {
+                                        var a = IPython.notebook.get_cell(0);
+                                        a.focus_cell();
+                                        IPython.notebook.execute_cell(0);
+                                    };
+                                    """)
+           
     def page_click(self,button):
         page = button.from_page
         this_page = getattr(self,page)
@@ -97,7 +126,6 @@ class browser_hud:
                 if edit_dict.has_key(it.description):
                     it.value = edit_dict[it.description]
                     
-    
     def make_general_page(self,page_title='General'):
         
         def dropdown_check(choice):
@@ -135,7 +163,14 @@ class browser_hud:
                                      from_page=page_title)
         edit_button.on_click(self.page_click)
 
-        
+
+        color_scheme =  {'background_color': '#75715e',
+                         'color': '#a6e22e',
+                         'font_family': u'verdana',
+                         'font_size': u'14px',
+                         'font_weight': u'bold'}
+        self.set_color_scheme(color_scheme,[add_button,edit_button])
+
 
         category = widgets.Dropdown(description='Category',
                                     options=['Not Specified','(New)'])
@@ -151,15 +186,7 @@ class browser_hud:
         col4 = edit_button
         col4.margin =  '5px 0px 5px 0px'
 
-
         row = widgets.HBox([col1,col2,col3,col4])
-
-        # row1 = widgets.HBox(children=[ add_button,cmt,score,category ],
-        #                     pack='start',align='start',
-        #                     padding='10px 0px 10px 0px')
-        # row2 = widgets.HBox(children=[ saved_comments,edit_button ],
-        #                     padding='10px 0px 10px 0px')
-        # rows = widgets.VBox(children=[ row1,row2 ],align='center')
         return widgets.Box(children=[row],description=page_title)
 
     def make_trait_scoring_page(self,page_title='Traits'):
@@ -181,29 +208,24 @@ class browser_hud:
                                        max=5,
                                        _dom_classes=['score'])
         score.width = '50px'
-        trait = widgets.Text(description='Trait',disable=True)
+        trait = widgets.Text(description='Trait',value='', disabled=True)
         update_button = widgets.Button(description='UPDATE',
                                        tooltip='Increase Points',
                                        from_page=page_title)
         update_button.on_click(self.page_click)
-        
-        # r1_col1 = update_button
-        # r1_col1.margin = '5px 25px 0px 5px'
-        # cmt.margin = '0px 0px 5px 0px'
-        # score.margin = '0px 0px 5px 0px'
-        # r1_col2 = cmt
-        # # widgets.VBox([ cmt,score,trait ])
-        # r1_col2.margin = '5px 0px 5px 0px'
-        # # new_traits.height = '81px'
-        # # new_traits.margin = '0px 5px 5px 0px'
-        # # scored_traits.margin = '0px 5px 0px 0px'
-        # # scored_traits.height = '81px'
-        # r1_col3 = new_traits
+        fake_button = widgets.Button(description='UPDATE',_dom_classes = ['update_btn_1'])
+
+
+        color_scheme =  {'background_color': '#75715e',
+                         'color': '#a6e22e',
+                         'font_family': u'verdana',
+                         'font_size': u'14px',
+                         'font_weight': u'bold'}
+        self.set_color_scheme(color_scheme,[score_button,edit_button,update_button,fake_button])
 
         r1_col1 = update_button
         r1_col1.margin = '5px 25px 0px 5px'
-        r2_col1 = widgets.Button(description='UPDATE')
-        r2_col1._dom_classes = ['update_btn_1']
+        r2_col1 = fake_button
         r2_col1.margin = r1_col1.margin
         
         cmt.height = '81px'
@@ -213,7 +235,6 @@ class browser_hud:
 
         score.margin = '0px 0px 10px 0px'
         r2_col2 = widgets.VBox([ score,trait ])
-        # r2_col2.pack = 'baseline'
         r2_col2.margin = r1_col2.margin
         r2_col2.height = cmt.height
 
@@ -231,21 +252,15 @@ class browser_hud:
         r1_col4 = widgets.VBox([score_button])
         r1_col4.width = '95px'
         r1_col4.margin = '5px 0px 0px 0px'
+        r1_col4.pack='start'
+        r1_col4.align='end'
+
         r2_col4 = widgets.VBox([edit_button])
         r2_col4.width = r1_col4.width
         r2_col4.height = r2_col3.height
         r2_col4.orientation='horizontal'
         r2_col4.pack='end'
         r2_col4.align='start'
-
-        
-        
-        
-
-        # col1 = widgets.VBox([r1_col1,r2_col1])
-        # col2 = widgets.VBox([r1_col2,r2_col2])
-        # col3 = widgets.VBox([r1_col3,r2_col3])
-        # col4 = widgets.VBox([r1_col4,r2_col4])
 
         row1_a = widgets.HBox([r1_col1,r1_col2])
         row2_a = widgets.HBox([r2_col1,r2_col2])
@@ -254,62 +269,25 @@ class browser_hud:
 
         row1_b = widgets.HBox([r1_col3,r1_col4])
         row1_b.pack='end'
-        row1_b.align='end'
+        row1_b.align='start'
         row2_b = widgets.HBox([r2_col3,r2_col4])
-        # row2_b._dom_classes = ['row2_b']
         row2_b.pack='end'
         row2_b.align='end'
-
-        
-
 
         col_b = widgets.VBox([row1_b,row2_b])
         col_b.pack='end'
         col_b.align='end'
-        # col_b._dom_classes = ['col_b']
-        # col_b.pack='end'
-        # col_b.align='start'
 
         row = widgets.HBox([col_a,col_b])
-        # new_box = widgets.HBox([new_traits,score_button])
-        # new_box.align = 'stretch'
-        # stored_box = widgets.HBox([scored_traits,edit_button])
-        # stored_box.align = 'stretch'
-        # col3 = widgets.VBox([new_box,stored_box])
-
-
-
-        # col3 = widgets.VBox([new_traits,scored_traits])
-        # col3.margin =  '5px 0px 5px 25px'
-        # col3.align='end'
-        # col4 = widgets.VBox([score_button,edit_button])
-        # col4.margin =  '5px 0px 5px 0px'
-        # # col4.align='end'
-        # col4.orientation='vertical'
-        # col4.pack='stretch'
-
-
-
-
-        # row = widgets.HBox([col1,col2,col3,col4])
-        # col = widgets.VBox([row1,row2])
-
-
-        # row1 = widgets.HBox(children=[ new_traits,score_button,scored_traits,edit_button ],
-        #                     padding='10px 0px 10px 0px')
-        # row2 = widgets.HBox(children=[ cmt,score,trait,update_button ],
-        #                     pack='start',align='start',
-        #                     padding='10px 0px 10px 0px')
-        # rows = widgets.VBox(children=[ row1,row2 ],align='center')
-
         return widgets.Box(children=[row],description=page_title)
 
     def make_tabs(self,_pages):
         tabs = widgets.Tab(children=_pages)
-        tabs.background_color = '#fff'
-        tabs.border_color = '#ccc'
-        tabs.border_width = 1
-        tabs.border_radius = 5
+        tabs.background_color='#444648'
+        tabs.font_family='verdana'
+        tabs.font_weight='bold'
+        tabs.font_size='14px'
+        tabs.color='#DC44FF'
         
         for i in range(len(tabs.children)):
             it=tabs.children[i]
@@ -326,8 +304,8 @@ class browser_hud:
             elif b.description.lower().count('next'):
                 self.load_url('next')
             elif b.description.lower().count('exit'):
-                self._widget.close()
-        
+                self.close_widget()
+
         prev_button=widgets.Button(description='<<--  PREV',
                                    margin = '0% 2% 0% 2%',
                                    background_color='#3BBAF5',
@@ -346,8 +324,17 @@ class browser_hud:
                                    margin = '0% 0% 0% 70%',
                                    background_color = '#FA7676',
                                    padding = 4,
-                                   _dom_classes=['exit_btn'])                          
+                                   _dom_classes=['exit_btn'])
         exit_button.on_click(click)
+
+        color_scheme = {'background_color':'#D95D18',
+                        'font_family':'verdana',
+                        'font_weight':'bold',
+                        'font_size':'14px',
+                        'color':'#f8f8f2'}
+        for it in [prev_button,next_button,exit_button]:
+            for k,v in color_scheme.iteritems():
+                setattr(it,k,v)
         
         menu_bar = widgets.HBox(children=[ prev_button,next_button,exit_button ],
                                 padding=4,
@@ -360,29 +347,72 @@ class browser_hud:
         x.children = _children
         x._dom_classes = ['inspector']
     
-    def remake_widget(self,children):
-        self._widget.__init__(children)
+    def remake_widget(self,children=[]):
+        if not children:
+            self._widget.__init__(self._widget.children)
+        else:
+            self._widget.__init__(children)
 
     def display_widget(self):
+        self.set_css()
         display(self._widget)
-        get_ipython().run_cell_magic(u'javascript', u'', 
-                                     u"""$('div.inspector').detach().prependTo($('body'))
-                                         .css({ 'z-index': 999,           
-                                                position: 'fixed',
-                                                'box-shadow': '5px 5px 12px 3px black',
-                                                'min-width': '50%',
-                                                opacity: 0.9, 
-                                                width: '85%', 
-                                                height: '300px', 
-                                                left: '7.5%',
-                                                bottom: '50px'
-                                                }).draggable({scroll: 'true'}).resizable();""")
+        if self.display_type=='float':
+            get_ipython().run_cell_magic(u'javascript', u'', 
+                                         u"""$('div.inspector').detach().prependTo($('body'))
+                                             .css({ 'z-index': 999,           
+                                                    position: 'fixed',
+                                                    'box-shadow': '5px 5px 12px 3px black',
+                                                    'min-width': '50%',
+                                                    opacity: 0.9, 
+                                                    width: '1200px', 
+                                                    height: '350px', 
+                                                    left: '7.5%',
+                                                    bottom: '50px'
+                                                    }).draggable({scroll: 'true'}).resizable();""")
+        elif self.display_type=='pager':
+            get_ipython().run_cell_magic(u'javascript', u'', 
+                                         u"""$('#pager').height('280px');$('#pager').show();""")
+            get_ipython().run_cell_magic(u'javascript', u'',u"$('div.inspector').prependTo($('#pager-container'));" )
+            get_ipython().run_cell_magic(u'javascript', u'',u"$('#pager-container > pre').hide();")
+        self.set_css()
+        if hasattr(self,'start_container'):
+            self.start_container.close()                         
     
-    def close(self):
+    def close_widget(self,display_type=''):
+        get_ipython().run_cell_magic(u'javascript', u'',u"$('#pager').hide();")
         self._widget.close()
+        self.create_start_hud_button(self.display_type)
         
+    def create_start_hud_button(self,display_type):
+        self.start_button = widgets.Button(description='START HUD')
+        self.start_button.background_color = '#30FA34'
 
-    def start(self):
+        def click_start_hud_btn(b):
+            self.display_type = self.start_container.display_type
+            self.start_container.close()
+            self.start_button.close()
+            get_ipython().run_cell_magic(u'javascript', u'',u"$('div.start_button_container').remove();")
+            self.display_type = 'float' if self.display_type=='pager' else 'pager'
+            browser_hud(self.display_type).start_hud()
+
+        self.start_button.on_click(click_start_hud_btn)
+
+        self.start_container = widgets.Box([self.start_button],_dom_classes=['start_button_container'])
+        self.start_container.height='40px'
+        self.start_container.width='70px'
+        self.start_container.display_type = self.display_type
+
+        display(self.start_container)
+        get_ipython().run_cell_magic(u'javascript', u'', 
+                                                 u"""$('div.start_button_container').detach().prependTo($('body'))
+                                                     .css({ 'z-index': 999,           
+                                                            position: 'fixed',
+                                                            bottom: '10%',
+                                                            left: '50px',
+                                                            });""")
+
+
+    def start_hud(self):
         self.General = self.make_general_page(page_title='General')
         self.Work = self.make_trait_scoring_page(page_title='Work')
         self.School = self.make_trait_scoring_page(page_title='School')
